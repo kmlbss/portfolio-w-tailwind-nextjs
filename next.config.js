5c105e4bf088c70d6dc812f4345203649d244d82
kmlbss <49821804+kmlbss@users.noreply.github.com>
2022-10-18 01:22:15 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    loader:"akamai",
+    path:"",
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
