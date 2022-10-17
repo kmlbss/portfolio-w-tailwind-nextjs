b7c3c5b55dcb1e6f43c29543cceb6883254ba8f7
kmlbss <49821804+kmlbss@users.noreply.github.com>
2022-10-18 02:00:41 +0400

    s

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    loader:"akamai",
-    path:"",
+  images: {
+    loader: "akamai",
+    path: "",
   },
-  basePath:"/portfolio-w-tailwind-nextjs",
-  assetPrefix:"/portfolio-w-tailwind-nextjs",
+  basePath: "/portfolio-w-tailwind-nextjs",
+  assetPrefix: "/portfolio-w-tailwind-nextjs",
   swcMinify: true,
 };
 
