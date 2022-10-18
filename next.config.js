02fd17307bef5fe72363ab0e52901533fb739e0b
kmlbss <49821804+kmlbss@users.noreply.github.com>
2022-10-19 00:38:44 +0400

    imgpath

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     loader: "akamai",
     path: "/portfolio-w-tailwind-nextjs",
+    domains: ["https://kmlbss.github.io/portfolio-w-tailwind-nextjs"],
   },
   basePath: "/portfolio-w-tailwind-nextjs",
   assetPrefix: "/portfolio-w-tailwind-nextjs",
